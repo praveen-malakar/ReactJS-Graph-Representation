1772dd3450a9094ad946e0c6aa636d18848938c8
Praveen Malakar <praveenmalakar1996@gmail.com>
2018-10-24 19:19:26 +0530

    - Updated component html with bootstrap classes.

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -11,57 +11,67 @@ class Charts extends Component {
     }
 
     static defaultProps = {
-        displayTitle:true,
-        displayLegend:true,
-        legendPosition:'bottom',
-        location:'city'
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: 'bottom',
+        location: 'city'
     }
 
     render() {
         return (
             <div className="charts">
-                <Bar
-                    data={this.state.chartData}
-                    options={{
-                        title:{
-                            display:this.props.displayTitle,
-                            text:'Maximum '+this.props.location+' in Chart',
-                            fontSize:25
-                        },
-                        legend:{
-                            display:this.props.displayLegend,
-                            position:this.props.legendPosition
-                        }
-                    }}
-                />
-                <Line
-                    data={this.state.chartData}
-                    options={{
-                        title:{
-                            display:this.props.displayTitle,
-                            text:'Maximum '+this.props.location+' in Chart',
-                            fontSize:25
-                        },
-                        legend:{
-                            display:this.props.displayLegend,
-                            position:this.props.legendPosition
-                        }
-                    }}
-                />
-                <Pie
-                    data={this.state.chartData}
-                    options={{
-                        title:{
-                            display:this.props.displayTitle,
-                            text:'Maximum '+this.props.location+' in Chart',
-                            fontSize:25
-                        },
-                        legend:{
-                            display:this.props.displayLegend,
-                            position:this.props.legendPosition
-                        }
-                    }}
-                />
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <Pie
+                                data={this.state.chartData}
+                                options={{
+                                    title: {
+                                        display: this.props.displayTitle,
+                                        text: 'Maximum ' + this.props.location + ' in Pie Chart',
+                                        fontSize: 25
+                                    },
+                                    legend: {
+                                        display: this.props.displayLegend,
+                                        position: this.props.legendPosition
+                                    }
+                                }}
+                            />
+                        </div>
+                        <div className="col-sm-6">
+                            <Line
+                                data={this.state.chartData}
+                                options={{
+                                    title: {
+                                        display: this.props.displayTitle,
+                                        text: 'Maximum ' + this.props.location + ' in Line Chart',
+                                        fontSize: 25
+                                    },
+                                    legend: {
+                                        display: this.props.displayLegend,
+                                        position: this.props.legendPosition
+                                    }
+                                }}
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            <Bar
+                                data={this.state.chartData}
+                                options={{
+                                    title: {
+                                        display: this.props.displayTitle,
+                                        text: 'Maximum ' + this.props.location + ' in Bar Chart',
+                                        fontSize: 25
+                                    },
+                                    legend: {
+                                        display: this.props.displayLegend,
+                                        position: this.props.legendPosition
+                                    }
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
