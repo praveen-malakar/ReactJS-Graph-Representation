525476f620d6b87ffc75fdd7366cd7b77966e3cf
Praveen Malakar <praveenmalakar1996@gmail.com>
2018-10-24 18:06:24 +0530

    - Added chart data and call it in app component.

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,1 +1,70 @@
 import React, {Component} from 'react'
+import {Bar, Line, Pie} from 'react-chartjs-2'
+
+class Charts extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            chartData: props.chartData
+
+        }
+    }
+
+    static defaultProps = {
+        displayTitle:true,
+        displayLegend:true,
+        legendPosition:'bottom',
+        location:'city'
+    }
+
+    render() {
+        return (
+            <div className="charts">
+                <Bar
+                    data={this.state.chartData}
+                    options={{
+                        title:{
+                            display:this.props.displayTitle,
+                            text:'Maximum '+this.props.location+' in Chart',
+                            fontSize:25
+                        },
+                        legend:{
+                            display:this.props.displayLegend,
+                            position:this.props.legendPosition
+                        }
+                    }}
+                />
+                <Line
+                    data={this.state.chartData}
+                    options={{
+                        title:{
+                            display:this.props.displayTitle,
+                            text:'Maximum '+this.props.location+' in Chart',
+                            fontSize:25
+                        },
+                        legend:{
+                            display:this.props.displayLegend,
+                            position:this.props.legendPosition
+                        }
+                    }}
+                />
+                <Pie
+                    data={this.state.chartData}
+                    options={{
+                        title:{
+                            display:this.props.displayTitle,
+                            text:'Maximum '+this.props.location+' in Chart',
+                            fontSize:25
+                        },
+                        legend:{
+                            display:this.props.displayLegend,
+                            position:this.props.legendPosition
+                        }
+                    }}
+                />
+            </div>
+        )
+    }
+}
+
+export default Charts
